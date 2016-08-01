16336d7b983e0a957b777e2138dd77689c1c7e2d
Zhao Yun <zjlyzhy@gmail.com>
2016-08-02 00:01:50 +0800

    add async-bytecomp.

--- modules/init-utils.el
+++ modules/init-utils.el
@@ -1,6 +1,7 @@
 (defconst my/utils-package
   '(restclient
     sunrise-commander
+    async
     ))
 
 (install-packages my/utils-package)
@@ -11,6 +12,12 @@
 (use-package sunrise-commander
   :commands (sunrise)
   :bind ("C-x t c" . sunrise))
+
+(use-package async-bytecomp
+  :ensure async
+  :init
+  (async-bytecomp-package-mode))
+
 
 (provide 'init-utils)
 ;;; init-utils.el ends here.
