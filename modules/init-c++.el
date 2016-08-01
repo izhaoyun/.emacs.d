a4a839ad8b983a042ae32c90c1e764e978adac38
Zhao Yun <zjlyzhy@gmail.com>
2016-08-02 00:00:24 +0800

    update rtags settings.

--- modules/init-c++.el
+++ modules/init-c++.el
@@ -4,10 +4,12 @@
     function-args
     rtags
     irony
+    company-irony
+    flycheck-irony
+    ggtags
     ))
 
 (install-packages my/c++-packages)
-
 
 (use-package cc-mode
   :mode ("\\.h\\(h?\\|xx\\|pp\\)\\'" . c++-mode)
@@ -28,6 +30,55 @@
 	       (diminish 'hs-minor-mode)))
   )
 
+(use-package irony
+  :init
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  :config
+  (defun my-irony-mode-hook ()
+    (define-key irony-mode-map [remap completion-at-point]
+      'irony-completion-at-point-async)
+    (define-key irony-mode-map [remap complete-symbol]
+      'irony-completion-at-point-async))
+  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+  )
+
+(use-package company-irony
+  :init
+  (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+  :config
+  (eval-after-load 'company
+    '(add-to-list 'company-backends 'company-irony))
+  )
+
+(defun my/init-rtags ()
+  (require 'rtags)
+  (require 'company-rtags)
+
+  (rtags-start-process-unless-running)
+  
+  (setq rtags-completions-enabled t)
+  (eval-after-load 'company
+    '(add-to-list 'company-backends 'company-rtags))
+
+  (rtags-enable-standard-keybindings c-mode-base-map)
+
+  (define-key c-mode-base-map (kbd "M-.") 'rtags-find-symbol-at-point)
+  (define-key c-mode-base-map (kbd "M-,") 'rtags-find-references-at-point)
+  (define-key c-mode-base-map (kbd "M-;") 'rtags-find-file)
+  (define-key c-mode-base-map (kbd "C-.") 'rtags-find-symbol)
+  (define-key c-mode-base-map (kbd "C-,") 'rtags-find-references)
+  (define-key c-mode-base-map (kbd "C-<") 'rtags-find-virtuals-at-point)
+  (define-key c-mode-base-map (kbd "M-i") 'rtags-imenu)
+  )
+(add-hook 'c-mode-common-hook 'my/init-rtags)
+
+
+(defun my/init-company-c-headers ()
+  (eval-after-load 'company
+    '(add-to-list 'company-backends 'company-c-headers))
+  )
 
 (provide 'init-c++)
 ;; init-c++.el ends here.
