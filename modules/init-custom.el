c43f0866cecc190d9467c4e446b8bfea3492af20
Zhao Yun <zjlyzhy@gmail.com>
2016-08-02 23:10:11 +0800

    update frame title format.

--- modules/init-custom.el
+++ modules/init-custom.el
@@ -14,4 +14,11 @@
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
+;; more useful frame title, that show either a file or a buffer name
+;; (if the buffer isn 't visiting a file)
+(setq frame-title-format
+      '((:eval (if (buffer-file-name)
+                   (abbreviate-file-name (buffer-file-name))
+                 "%b"))))
+
 (provide 'init-custom)
