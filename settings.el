6faa3ccec84bffe8541e9c6533e8254c6904cc28
Zhao Yun <zjlyzhy@gmail.com>
2016-08-06 12:21:35 +0800

    update settings.el.

--- settings.el
+++ settings.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(backup-directory-alist (quote (("." . "~/.emacs.d/backups"))))
+ '(inhibit-startup-message t)
+ '(column-number-mode t)
  '(menu-bar-mode nil)
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil)
