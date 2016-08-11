a5fe2e97e340e66c40f95e07d404a22cc847b474
Zhao Yun <zjlyzhy@gmail.com>
2016-08-11 21:15:05 +0800

    update init-python.el

--- modules/init-python.el
+++ modules/init-python.el
@@ -14,9 +14,13 @@
   (setq python-indent-guess-indent-offset nil)
   )
 
-(use-package elpy
-  :init
-  (add-hook 'python-mode-hook 'elpy-enable)
+(defun python/init-elpy ()
+  (use-package elpy
+	:init
+	(elpy-enable)
+	)
   )
+(add-hook 'python-mode-hook 'python/init-elpy)
+
 
 (provide 'init-python)
