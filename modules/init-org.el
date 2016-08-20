263583acd57f390b3a5f9cb866b28f82a8565ccb
Zhao Yun <zjlyzhy@gmail.com>
2016-08-21 02:58:33 +0800

    update settings for exporting html from org files.

--- modules/init-org.el
+++ modules/init-org.el
@@ -30,131 +30,120 @@
 			  " \t\r\n,\"'"
 			  "."
 			  1))
-  :config
-  (setq org-src-fontify-natively t)
-  (setq org-src-tab-acts-natively t)
-  (setq org-src-preserve-indentation t)
-
-  (add-to-list 'org-latex-packages-alist '("" "ctex"))
-  (add-to-list 'org-latex-packages-alist '("" "minted"))
-  (add-to-list 'org-latex-packages-alist '("" "color"))
-  (add-to-list 'org-latex-packages-alist '("" "geometry"))
-  (add-to-list 'org-latex-packages-alist '("" "tabularx"))
-  (add-to-list 'org-latex-packages-alist '("" "tabu"))
-  (add-to-list 'org-latex-packages-alist '("" "fancyhdr"))
-  (add-to-list 'org-latex-packages-alist '("" "natbib"))
-  (add-to-list 'org-latex-packages-alist '("" "titlesec"))
-
-  (add-hook 'org-mode-hook 'my/init-org-export)
-
-  ;; (setcar (nthcdr 0 org-emphasis-regexp-components) " \t('\"{[:nonascii:]")
-  ;; (setcar (nthcdr 1 org-emphasis-regexp-components) "- \t.,:!?;'\")}\\[[:nonascii:]")
-  ;; (org-set-emph-re 'org-emphasis-regexp-components org-emphasis-regexp-components)
-  ;; (org-element-update-syntax)
 
   (setq org-file-apps '((auto-mode . emacs)
 						("\\.mm\\'" . default)
 						("\\.x?html?\\'" . default)
 						("\\.pdf\\'" . "evince %s")))
-  )
 
-(use-package org-bullets
-  :init
-  (add-hook 'org-mode-hook 'org-bullets-mode)
-  )
+  (setq org-src-fontify-natively t)
+  (setq org-src-tab-acts-natively t)
+  (setq org-src-preserve-indentation t)
 
-(use-package ob
   :config
-  (setq org-confirm-babel-evaluate nil)
-  (add-hook 'org-babel-after-execute-hook 'org-display-inline-images 'append)
 
-  (use-package ob-plantuml
-  	:config
-  	(setq org-plantuml-jar-path
-		  (shell-command-to-string "locate plantuml.jar"))
-  	)
-
-  (use-package ob-gnuplot
+  (use-package ox
 	:init
-	(use-package gnuplot
-	  :mode ("\\.\\(gp\\|gnuplot\\)$" . gnuplot-mode)
-	  )
-	)
-
-  (use-package ob-dot
-	:init
-	(use-package graphviz-dot-mode
-	  :mode ("\\.dot\\'" . graphviz-dot-mode)
-	  )
-	)
-
-  (use-package ob-ditaa
+    (setq org-export-default-language "zh-CN")
 	:config
-	;; get the location of ditaa*.jar.
-	(setq org-ditaa-jar-path
-		  (shell-command-to-string "locate ditaa | grep '\.jar$' | grep '/usr/share'"))
-	)
+	(use-package ox-beamer)
+	(use-package ox-gfm)
+	(use-package ox-html
+	  :config
+	  (defadvice org-html-paragraph (before org-html-paragraph-advice
+											(paragraph contents info) activate)
+		"Join consecutive Chinese lines into a single long line
+without unwanted space when exporting org-mode to html."
+		(let* ((origin-contents (ad-get-arg 1))
+			   (fix-regexp "[[:multibyte:]]")
+			   (fixed-contents
+				(replace-regexp-in-string
+				 (concat
+				  "\\(" fix-regexp "\\) *\n *\\(" fix-regexp "\\)") "\\1\\2" origin-contents)))
 
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((C          . t)
-	 (awk        . t)
-	 (dot        . t)
-	 (sed        . t)
-	 (sql        . t)
-	 (calc       . t)
-	 (ruby       . t)
-	 (ditaa      . t)
-	 (latex      . t)
-	 (shell      . t)
-	 (python     . t)
-	 (plantuml   . t)
-	 (emacs-lisp . t)
-	 (http       . t)
-	 (gnuplot    . t)
+		  (ad-set-arg 1 fixed-contents))
+		)
+	  )
+	(use-package ox-latex
+      :init
+      (setq org-latex-listings 'minted)
+      (setq org-latex-minted-options '(("frame"      "single")
+                                       ("breaklines" "")))
+      (setq org-latex-pdf-process
+            '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+              "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+              "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+
+	  (add-to-list 'org-latex-packages-alist '("" "ctex"))
+	  (add-to-list 'org-latex-packages-alist '("" "minted"))
+	  (add-to-list 'org-latex-packages-alist '("" "color"))
+	  (add-to-list 'org-latex-packages-alist '("" "geometry"))
+	  (add-to-list 'org-latex-packages-alist '("" "tabularx"))
+	  (add-to-list 'org-latex-packages-alist '("" "tabu"))
+	  (add-to-list 'org-latex-packages-alist '("" "fancyhdr"))
+	  (add-to-list 'org-latex-packages-alist '("" "natbib"))
+	  (add-to-list 'org-latex-packages-alist '("" "titlesec"))
+	  )
+    )
+
+  (use-package ob
+	:init
+	(setq org-confirm-babel-evaluate nil)
+	:config
+	(add-hook 'org-babel-after-execute-hook 'org-display-inline-images 'append)
+
+	(use-package ob-plantuml
+	  :init
+	  (setq org-plantuml-jar-path
+			(shell-command-to-string "locate plantuml.jar"))
+	  )
+
+	(use-package ob-gnuplot
+	  :init
+	  (use-package gnuplot
+		:mode ("\\.\\(gp\\|gnuplot\\)$" . gnuplot-mode)
+		)
+	  )
+
+	(use-package ob-dot
+	  :init
+	  (use-package graphviz-dot-mode
+		:mode ("\\.dot\\'" . graphviz-dot-mode)
+		)
+	  )
+
+	(use-package ob-ditaa
+	  :init
+	  ;; get the location of ditaa*.jar.
+	  (setq org-ditaa-jar-path
+			(shell-command-to-string "locate ditaa | grep '\.jar$' | grep '/usr/share'"))
+	  )
+
+	(org-babel-do-load-languages
+	 'org-babel-load-languages
+	 '((C          . t)
+	   (awk        . t)
+	   (dot        . t)
+	   (sed        . t)
+	   (sql        . t)
+	   (calc       . t)
+	   (ruby       . t)
+	   (ditaa      . t)
+	   (latex      . t)
+	   (shell      . t)
+	   (python     . t)
+	   (plantuml   . t)
+	   (emacs-lisp . t)
+	   (http       . t)
+	   (gnuplot    . t)
+	   )
 	 )
-   )
-  )
-
-;;;###autoload
-(defun my/init-org-export ()
-
-  (require 'ox-beamer)
-  (require 'ox-gfm)
-
-  (use-package ox-html)
-
-  (use-package ox-latex
-	:config
-	(setq org-latex-listings 'minted)
-	(setq org-latex-minted-options '(("frame"      "single")
-									 ("breaklines" "")))
-	(setq org-latex-pdf-process
-		  '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-			"xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
-			"xelatex -shell-escape -interaction nonstopmode -output-directory %o %f")))
-  )
-
-(use-package ox
-  :preface
-  (defun clear-single-linebreak-in-cjk-string (string)
-    (let* ((regexp "\\([\u4E00-\u9FA5]\\)\n\\([\u4E00-\u9FA5]\\)")
-           (start (string-match regexp string)))
-      (while start
-        (setq string (replace-match "\\1\\2" nil nil string)
-              start (string-match regexp string start))))
-    string)
-  :commands (org-export-derived-backend-p)
-  :init
-  (defun ox-html-clear-single-linebreak-for-cjk (string backend info)
-    (when (org-export-derived-backend-p backend 'html)
-      (clear-single-linebreak-in-cjk-string string))
 	)
-  :config
-  (setq org-export-default-language "zh-CN")
-  ;; (setq org-latex-compiler "xelatex")
-  (add-to-list 'org-export-filter-final-output-functions
-			   'ox-html-clear-single-linebreak-for-cjk)
+
+  (use-package org-bullets
+	:init
+	(add-hook 'org-mode-hook 'org-bullets-mode)
+	)
   )
 
 (provide 'init-org)
