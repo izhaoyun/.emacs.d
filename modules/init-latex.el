0221b4ec2cda58ffcffa1f2d81c71d819f533944
Zhao Yun <zjlyzhy@gmail.com>
2017-03-21 23:41:26 +0800

    update latex configuration

--- modules/init-latex.el
+++ modules/init-latex.el
@@ -1,5 +1,8 @@
-;;; -*- lexical-binding: t; -*-
+;;; init-latex --- LaTeX Settings ----*- lexical-binding: t; -*-
 
+;;; Commentary:
+
+;;; Code:
 (use-package auctex
   :mode ("\\.tex\\'" . latex-mode)
   :init
@@ -7,16 +10,18 @@
   (add-hook 'LaTeX-mode-hook #'flyspell-mode)
   (add-hook 'LaTeX-mode-hook #'turn-on-reftex)
   :config
-  (setq TeX-auto-save t)
-  (setq TeX-parse-self t)
+  (setq-default TeX-auto-save t)
+  (setq-default TeX-parse-self t)
   (setq-default TeX-master nil))
 
 (use-package reftex
   :commands turn-on-reftex
   :init
-  (setq reftex-plug-into-AUCTeX t))
+  (setq reftex-plug-into-AUCTeX t)
+  (add-hook 'LaTeX-mode-hook 'turn-on-reftex))
 
 (use-package preview
   :commands LaTeX-preview-setup)
 
 (provide 'init-latex)
+;;; init-latex.el ends here
