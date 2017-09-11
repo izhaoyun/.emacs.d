090a08b08a574d57b4af33785388dd2229ca38e0
Zhao Yun <zjlyzhy@gmail.com>
2017-09-12 00:59:03 +0800

    add anaconda-mode and company-anaconda for python programming.

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -3,4 +3,18 @@
   :interpreter ("python" . python-mode)
   )
 
+;; @github: proofit404/anaconda-mode
+(use-package anaconda-mode
+  :init
+  (add-hook 'python-mode-hook 'anaconda-mode)
+  :config
+  (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+  )
+
+;; @github: proofit404/company-anaconda
+(use-package company-anaconda
+  :init
+  (push 'company-anaconda company-backends)
+  )
+
 (provide 'init-python)
