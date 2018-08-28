be9ebff12cf569de25ccec8380c443f499e921ea
Zhao Yun <zjlyzhy@gmail.com>
2018-08-28 21:03:54 +0800

    added go-impl

--- custom.el
+++ custom.el
@@ -21,13 +21,13 @@
  '(max-specpdl-size 5000)
  '(menu-bar-mode nil)
  '(package-archives
-   (quote
-    (("melpa" . "https://melpa.org/packages/")
-     ("marmalade" . "https://marmalade-repo.org/packages/")
-     ("gnu" . "https://elpa.gnu.org/packages/"))))
+ (quote
+  (("melpa" . "https://melpa.org/packages/")
+   ("marmalade" . "https://marmalade-repo.org/packages/")
+   ("gnu" . "https://elpa.gnu.org/packages/"))))
  '(package-selected-packages
-   (quote
-    (ranger cd-compile delight flycheck-pos-tip go-gen-test go-fill-struct go-tag go-imports go-snippets go-direx popwin company-restclient restclient shell-pop auctex wgrep-ag eldoc-overlay auto-package-update system-packages rg use-package-chords use-package-ensure-system-package go-gopath volatile-highlights ansible-vault ace-pinyin vmd-mode markdown-mode protobuf-mode vagrant company-ansible ansible region-bindings-mode multiple-cursors ob-go go-rename easy-kill counsel-projectile counsel-gtags flycheck-clang-analyzer flycheck-yamllint systemd http yasnippet-snippets counsel-etags counsel-tramp eshell-bookmark gorepl-mode go-playground docker docker-compose-mode docker-tramp dockerfile-mode pyenv-mode magit ob-http flycheck-pkg-config clang-format company-lua lua-mode cython-mode json-mode diff-hl helm-make disaster modern-cpp-font-lock elf-mode demangle-mode py-autopep8 wgrep show-marks haskell-mode pyvenv company-auctex js2-mode flycheck-gometalinter pip-requirements importmagic company-anaconda anaconda-mode hydra ivy-hydra async go-guru go-dlv htmlize yasnippet company-erlang erlang yaml-mode web-mode graphviz-dot-mode plantuml-mode toc-org go-mode company-c-headers sphinx-doc go-eldoc company-go go-mode-autoloads irony irony-eldoc company-irony company-irony-c-headers flycheck-irony ggtags google-c-style cmake-font-lock cmake-mode lispy auto-compile flycheck rainbow-delimiters aggressive-indent dtrt-indent clean-aindent-mode highlight-indent-guides comment-dwim-2 company-quickhelp company undo-tree expand-region ws-butler which-key highlight-symbol window-numbering ace-link avy-zap avy counsel swiper ivy bind-key diminish use-package)))
+ (quote
+  (go-impl ranger cd-compile delight flycheck-pos-tip go-gen-test go-fill-struct go-tag go-imports go-snippets go-direx popwin company-restclient restclient shell-pop auctex wgrep-ag eldoc-overlay auto-package-update system-packages rg use-package-chords use-package-ensure-system-package go-gopath volatile-highlights ansible-vault ace-pinyin vmd-mode markdown-mode protobuf-mode vagrant company-ansible ansible region-bindings-mode multiple-cursors ob-go go-rename easy-kill counsel-projectile counsel-gtags flycheck-clang-analyzer flycheck-yamllint systemd http yasnippet-snippets counsel-etags counsel-tramp eshell-bookmark gorepl-mode go-playground docker docker-compose-mode docker-tramp dockerfile-mode pyenv-mode magit ob-http flycheck-pkg-config clang-format company-lua lua-mode cython-mode json-mode diff-hl helm-make disaster modern-cpp-font-lock elf-mode demangle-mode py-autopep8 wgrep show-marks haskell-mode pyvenv company-auctex js2-mode flycheck-gometalinter pip-requirements importmagic company-anaconda anaconda-mode hydra ivy-hydra async go-guru go-dlv htmlize yasnippet company-erlang erlang yaml-mode web-mode graphviz-dot-mode plantuml-mode toc-org go-mode company-c-headers sphinx-doc go-eldoc company-go go-mode-autoloads irony irony-eldoc company-irony company-irony-c-headers flycheck-irony ggtags google-c-style cmake-font-lock cmake-mode lispy auto-compile flycheck rainbow-delimiters aggressive-indent dtrt-indent clean-aindent-mode highlight-indent-guides comment-dwim-2 company-quickhelp company undo-tree expand-region ws-butler which-key highlight-symbol window-numbering ace-link avy-zap avy counsel swiper ivy bind-key diminish use-package)))
  '(save-interprogram-paste-before-kill t)
  '(scroll-bar-mode nil)
  '(sentence-end-double-space nil)
--- lisp/init-go.el
+++ lisp/init-go.el
@@ -22,15 +22,34 @@
     (setenv "PATH" (concat (getenv "PATH") ":" (concat gopath "/bin")))
     (setq exec-path (append exec-path (list (concat gopath "/bin"))))
     )
-
   :commands (gofmt-before-save)
   :hook
   ((go-mode . hs-minor-mode)
+   (go-mode . flycheck-mode)
    (go-mode . go/init-company)
    (go-mode . go/setup-env-var)
    (go-mode . (lambda ()
                 (add-hook 'before-save-hook 'gofmt-before-save)))
    )
+  :bind
+  (:map go-mode-map
+        ("C-c g a" . go-imports-insert-import)
+        ("C-c g p" . go-direx-pop-to-buffer)
+        ("C-c g b" . go-direx-switch-to-buffer)
+        ("C-c g i" . go-impl)
+        ("C-c g f" . go-fill-struct)
+        ("C-c g r" . go-rename)
+        ("C-c g l" . go-imports-reload-packages-list)
+        ("C-c g t" . go-tag-add)
+        ("C-c g v" . go-tag-remove)
+        ("C-c t g" . go-gen-test-dwim)
+        ("C-c t a" . go-gen-test-all)
+        ("C-c t e" . go-gen-test-exported)
+        ("C-c t f" . go-test-current-file)
+        ("C-c t t" . go-test-current-test)
+        ("C-c t p" . go-test-current-project)
+        ("C-c t b" . go-test-current-benchmark)
+        ("C-c t x" . go-run))
   )
 
 (use-package go-snippets
@@ -53,60 +72,11 @@
   (go-mode . go-guru-hl-identifier-mode)
   )
 
-(use-package go-rename
-  :bind
-  (:map go-mode-map
-        ("C-c t n" . go-rename))
-  )
-
-(use-package go-tag
-  :bind
-  (:map go-mode-map
-        ("C-c t a" . go-tag-add)
-        ("C-c t r" . go-tag-remove))
-  )
-
-(use-package go-direx
-  :bind
-  (:map go-mode-map
-        ("C-c t o" . go-direx-pop-to-buffer)
-        ("C-c t u" . go-direx-switch-to-buffer))
-  )
-
-(use-package go-imports
-  :bind
-  (:map go-mode-map
-        ("C-c t i" . go-imports-insert-import)
-        ("C-c t l" . go-imports-reload-packages-list))
-  )
-
-(use-package go-fill-struct
-  :bind
-  (:map go-mode-map
-        ("C-c t f" . go-fill-struct))
-  )
-
-(use-package gotest
-  :bind
-  (:map go-mode-map
-        ("C-c t c" . go-test-current-file)
-        ("C-c t t" . go-test-current-test)
-        ("C-c t p" . go-test-current-project)
-        ("C-c t b" . go-test-current-benchmark)
-        ("C-c t x" . go-run))
-  )
-
 (use-package go-playground)
 
 (use-package gorepl-mode
   :hook
   (go-mode . gorepl-mode)
-  )
-
-(use-package flycheck-gometalinter
-  :after flycheck
-  :config
-  (flycheck-gometalinter-setup)
   )
 
 (use-package protobuf-mode
